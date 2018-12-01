19ca4891c0f5fc22aa5a8dac894af4723d333fa6
Radek <kulikowskiradek@o2.pl>
2018-12-01 11:39:57 +0100

    [add] - dodanie klasy Car

---
+++ src/main/java/pl/rkul/my_git_test/Car.java
@@ -0,0 +1,20 @@
+package pl.rkul.my_git_test;
+
+public class Car {
+
+    String brand;
+    String model;
+
+    public Car(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
+}
