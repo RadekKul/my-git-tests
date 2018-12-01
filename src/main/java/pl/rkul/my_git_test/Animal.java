a6e6ae84b474cbd2ffb14d5ca7090bc41386eef0
Radek <kulikowskiradek@o2.pl>
2018-12-01 14:18:05 +0100

    [add] - new constructor in Dog

--- src/main/java/pl/rkul/my_git_test/Animal.java
+++ src/main/java/pl/rkul/my_git_test/Animal.java
@@ -4,7 +4,9 @@ public class Animal {
 
     String name;
 
-    public Animal(){ }
+    public Animal(String name) {
+        this.name = name;
+    }
 
     public String getName() {
         return name;
