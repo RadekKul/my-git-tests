8d9b9ab4e292fb6bd9985d7db7b1b479fec28a5a
Radek <kulikowskiradek@o2.pl>
2018-12-01 12:07:28 +0100

    [change] - Car to Auto

--- src/main/java/pl/rkul/my_git_test/Auto.java
+++ src/main/java/pl/rkul/my_git_test/Auto.java
@@ -1,18 +1,18 @@
 package pl.rkul.my_git_test;
 
-public class Car {
+public class Auto {
 
     String brand;
     String model;
 
-    public Car(String brand, String model) {
+    public Auto(String brand, String model) {
         this.brand = brand;
         this.model = model;
     }
 
     @Override
     public String toString() {
-        return "Car{" +
+        return "Auto{" +
                 "brand='" + brand + '\'' +
                 ", model='" + model + '\'' +
                 '}';
