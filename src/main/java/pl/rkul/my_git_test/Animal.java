13744f1958482cdd4f3fd64b98123569fcb37843
Radek <kulikowskiradek@o2.pl>
2018-12-01 13:08:06 +0100

    [add] - new code in Animal

--- src/main/java/pl/rkul/my_git_test/Animal.java
+++ src/main/java/pl/rkul/my_git_test/Animal.java
@@ -1,7 +1,16 @@
 package pl.rkul.my_git_test;
 
 public class Animal {
-    public Animal(){
 
+    String name;
+
+    public Animal(){ }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
